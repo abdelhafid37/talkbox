a129ad76797dfd097632faccae86bb0fc5213a02
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-07-08 19:04:03 +0100

    feat: add User model & connect to MongoDB Atlas

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,7 +5,7 @@ const MONGO_URI = process.env.MONGO_URI;
 async function connectDB() {
   try {
     await mongoose.connect(MONGO_URI);
-    console.log(`[DATABASE] Connected to MongoDB.`);
+    console.log("[DATABASE] Connected to MongoDB.");
   } catch (error) {
     console.error(`[DATABASE] Failed to connect to MongoDB: ${error.message}`);
     process.exit(1);
