8442d2cabc9749e07206d1f529d57bf06e6b0ffb
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-07-20 08:07:07 +0100

    feat(client): set up react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import ChatPage from "./pages/ChatPage";
+
 function App() {
-  return <h2>Talkbox</h2>;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/pages/ChatPage.jsx
@@ -0,0 +1,4 @@
+function ChatPage() {
+  return <h1>Chat Page</h1>;
+}
+export default ChatPage;
