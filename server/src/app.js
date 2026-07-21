01f1f62d4a41e538a059f9837fb1ca5660be1023
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-07-21 09:22:55 +0100

    feat(server): enable CORS for frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const authRoutes = require("./routes/authRoutes");
@@ -6,6 +8,11 @@ const userRoutes = require("./routes/userRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
