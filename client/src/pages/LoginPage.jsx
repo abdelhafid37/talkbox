fa83f314f248c86b8dd7b8b75fb7852e75e0b27c
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-07-21 09:24:16 +0100

    feat(auth): implement login page and API integration

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,16 +1,72 @@
-import useAuth from "../hooks/useAuth";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { login } from "@/services/authService";
 
 function LoginPage() {
-  const { user, token } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = await login({
+      email,
+      password,
+    });
+
+    console.log(data);
+  }
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <ul>
-        <li>User: {user ? user : "None"}</li>
-        <li>Token: {token ? token : "None"}</li>
-      </ul>
+    <div className="min-h-svh w-full flex items-center justify-center p-6 md:p-10">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Welcome back, login to your account to start chatting.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <FieldGroup>
+              <Field>
+                <FieldLabel htmlFor="email">Email</FieldLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="enter email"
+                />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="password">Password</FieldLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="enter password"
+                />
+              </Field>
+              <Field>
+                <Button type="submit">Login</Button>
+              </Field>
+            </FieldGroup>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
+
 export default LoginPage;
