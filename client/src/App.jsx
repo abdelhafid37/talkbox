023116dda8ce3724326904489027366d20589a96
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-07-21 20:24:22 +0100

    feat(auth): protect authenticated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ChatPage from "./pages/ChatPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -9,7 +10,14 @@ function App() {
       <Route path="/" element={<LoginPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/chat" element={<ChatPage />} />
+      <Route
+        path="/chat"
+        element={
+          <ProtectedRoute>
+            <ChatPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import useAuth from "@/hooks/useAuth";
+import { Navigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const { token } = useAuth();
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
