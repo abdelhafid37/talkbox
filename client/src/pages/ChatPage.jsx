4fdcba60752a62f26a0e6e860ed528c60fc19c4e
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-08-04 18:51:03 +0100

    feat(chat): complete realtime messaging fronend

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -56,6 +56,7 @@ export default function ChatPage() {
     if (!selectedUser) return;
 
     async function fetchConversation() {
+      setMessages([]);
       setIsMessagesLoading(true);
 
       try {
--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", { autoConnect: false });
+const socket = io(import.meta.env.VITE_API_URL, {
+  autoConnect: false,
+});
 
 export default socket;
